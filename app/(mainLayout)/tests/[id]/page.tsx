364b84e4080c822f4edd16453f78188c810e1d4b
Super7000 <subratachowdhurysuper@gmail.com>
2025-04-03 12:27:31 +0530

    fixed a bug related to server component test details fetching

--- app/(mainLayout)/tests/[id]/page.tsx
+++ app/(mainLayout)/tests/[id]/page.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import MainTestPage from './componenets/MainTestPage'
 import '@/styles/tiptap.css'
+import Test from '@/models/Test';
 
 interface TestPageProps {
     params: Promise<{ id: string }>; // Accessing the dynamic id from URL
 }
 
-export default async function Test({ params }: TestPageProps) {
+export default async function TestPage({ params }: TestPageProps) {
     const id = (await params).id;
-    const res = await fetch(`https://labtesto.vercel.app/api/tests/${id}`);
-    if (res.status === 200) {
-        const body = await res.json();
-        return <MainTestPage test={body} />
+    const res = await Test.findById(id)?.lean();
+    if (res) {
+        return <MainTestPage test={JSON.parse(JSON.stringify(res))} />
     }
     return <div>Test not found</div>
 }
