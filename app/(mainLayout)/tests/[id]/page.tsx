5efbc078e3e539b9a91ebb39d8e1cdcce6c23c97
Super7000 <subratachowdhurysuper@gmail.com>
2025-04-03 18:18:47 +0530

    now dbconnect before searching test in test/[id] route

--- app/(mainLayout)/tests/[id]/page.tsx
+++ app/(mainLayout)/tests/[id]/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import MainTestPage from './componenets/MainTestPage'
 import '@/styles/tiptap.css'
 import Test from '@/models/Test';
+import dbConnect from '@/config/db';
 
 interface TestPageProps {
     params: Promise<{ id: string }>; // Accessing the dynamic id from URL
@@ -9,6 +10,7 @@ interface TestPageProps {
 
 export default async function TestPage({ params }: TestPageProps) {
     const id = (await params).id;
+    await dbConnect();
     const res = await Test.findById(id)?.lean();
     if (res) {
         return <MainTestPage test={JSON.parse(JSON.stringify(res))} />
