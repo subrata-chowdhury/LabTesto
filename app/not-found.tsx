2ee4d305c0c8e46aad45f05b008385a90ca3f715
Super7000 <subratachowdhurysuper@gmail.com>
2025-05-02 13:04:53 +0530

    added custom not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import React from 'react'
+
+function NotFoundPage() {
+    return (
+        <main className="grid min-h-full h-screen place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+            <div className="text-center">
+                <p className="text-2xl font-semibold text-primary">404</p>
+                <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
+                    Page not found
+                </h1>
+                <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+                    Sorry, we couldn’t find the page you’re looking for.
+                </p>
+                <div className="mt-10 flex items-center justify-center gap-x-6">
+                    <Link
+                        href="/"
+                        className="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                        Go back home
+                    </Link>
+                    <Link href="/contact" className="text-sm font-semibold text-gray-900">
+                        Contact support <span aria-hidden="true">&rarr;</span>
+                    </Link>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default NotFoundPage
