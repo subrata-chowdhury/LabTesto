54dc428228fb821616eff98f1b63d3a60fe52623
Subrata Chowdhury <subratachowdhurysuper@gmail.com>
2025-10-30 08:24:29 +0530

    fixed a error in roboto.txt

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-    const BASE_URL = process.env.NEXT_PUBLIC_APP_URL;
+    const BASE_URL = process.env.NEXT_PUBLIC_APP_URL || "https://labtesto.vercel.app/";
 
     return {
         rules: [
